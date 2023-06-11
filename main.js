311cfe5a519dee17e08080e3ef88fd79e3c6316e
Nick <tortickk8475@gmail.com>
2023-06-11 23:56:56 +0300

    chore: add input to offer section

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const prevButton = document.querySelector('[data-btn="prevBtn"]');
 const nextButton = document.querySelector('[data-btn="nextBtn"]');
 const sliderBack = document.querySelector(".photo-slider__back");
 
+const offerInput = document.querySelector('[data-input="offer"]')
+
+
 let slideIndex = 0;
 
 showSlide(slideIndex);
@@ -38,4 +41,14 @@ function showSlide(n) {
 	slides[slideIndex].style.display = "block";
 }
 
+offerInput.addEventListener("focus", function() {
+	this.placeholder = "";
+});
 
+offerInput.addEventListener("blur", function() {
+	this.placeholder = "0";
+});
+
+offerInput.addEventListener("input", function() {
+	this.style.width = (this.value.length * 80) + "px";
+});
