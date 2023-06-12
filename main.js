45d8ad563b35a2596b012554372359a2d8bb92cc
Nick <tortickk8475@gmail.com>
2023-06-12 10:50:54 +0300

    chore: make select and toggle button

--- main.js
+++ main.js
@@ -6,8 +6,18 @@ const sliderBack = document.querySelector(".photo-slider__back");
 
 const offerInput = document.querySelector('[data-input="offer"]')
 
+const toggleCheckbox = document.querySelector('.toggle-btn__inp');
+const toggleElement = document.querySelector('.toggle-btn__movable-box');
+const toggleBtn = document.querySelector('.toggle-btn');
+const text1 = document.querySelector('.toggle-btn__text-1');
 
 let slideIndex = 0;
+
+window.onload = function() {
+	toggleCheckbox.checked = true;
+	offerInput.placeholder = "0";
+	offerInput.value = '';
+};
 
 showSlide(slideIndex);
 
@@ -51,4 +61,39 @@ offerInput.addEventListener("blur", function() {
 
 offerInput.addEventListener("input", function() {
 	this.style.width = (this.value.length * 80) + "px";
+});
+
+document.querySelector(".select-main__selected").addEventListener("click", function() {
+	let options = this.nextElementSibling;
+	options.style.display = options.style.display === "none" ? "block" : "none";
+});
+
+let options = document.querySelectorAll(".select-main__option");
+options.forEach(function(option) {
+	option.addEventListener("click", function() {
+		let selectedText = this.textContent;
+		let selectedValue = this.textContent;
+
+		document.querySelector(".select-main__selected").textContent = selectedText;
+		document.querySelector(".select-main__selected").dataset.value = selectedValue;
+
+		this.parentElement.style.display = "none";
+	});
+});
+
+toggleBtn.addEventListener('click', function() {
+	toggleElement.style.left = '48%';
+	toggleBtn.classList.add('toggle-btn--selected');
+	text1.style.color = '#111111';
+
+	if(!toggleCheckbox.checked) {
+		text1.style.fontWeight = '500';
+	}
+
+	if (toggleCheckbox.checked) {
+		toggleElement.style.left = '2%';
+		toggleBtn.classList.remove('toggle-btn--selected');
+		text1.removeAttribute('style');
+		text1.style.fontWeight = '600';
+	}
 });
